99486d9436e2280d47867fa59a716d2f51eb00d5
shirleywbi <swwbi@msn.com>
2020-08-30 01:37:20 -0700

    add navbar

---
+++ portfolio/src/components/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Image, Nav, Navbar } from 'react-bootstrap';
+import "../styles/Navigation.css"
+
+const Navigation = () => {
+    return <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+            <Image
+                className={"nav-logo"} 
+                src={require("../assets/images/logo.png")} 
+            />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className={"ml-auto"}>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#workexp">Work Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+}
+
+export default Navigation;
