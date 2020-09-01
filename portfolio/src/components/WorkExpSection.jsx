03e2cdbe5ab75c1041d3a4065aa5d82a3eee3631
shirleywbi <swwbi@msn.com>
2020-09-01 11:07:47 -0700

    format work exp section with container and row

--- portfolio/src/components/WorkExpSection.jsx
+++ portfolio/src/components/WorkExpSection.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import Section from './primitives/Section';
 import WorkExperience from './primitives/WorkExperience';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const WorkExpSection = ({id}) => {
     return <Section heading="Experience" id={id}>
-        <WorkExperience 
-            dateRange={"2020"}
-            logo={require("../assets/icons/fispan.png")} 
-            company="FISPAN" 
-            title="Software Developer Co-op"
-            description=""/>
-        <WorkExperience 
-            dateRange={"2019-2020"}
-            logo={require("../assets/icons/sap.svg")} 
-            company="SAP" 
-            title="Software Engineer Intern"
-            description=""/>
+        <Container>
+            <Row>
+                <Col>
+                    <WorkExperience 
+                        dateRange={"2020"}
+                        logo={require("../assets/icons/fispan.png")} 
+                        company="FISPAN" 
+                        title="Software Developer Co-op"
+                        description=""/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <WorkExperience 
+                        dateRange={"2019-2020"}
+                        logo={require("../assets/icons/sap.svg")} 
+                        company="SAP" 
+                        title="Software Engineer Intern"
+                        description=""/>
+                </Col>
+            </Row>
+        </Container>
     </Section>
 }
 
