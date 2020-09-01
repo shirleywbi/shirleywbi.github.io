dfe9220bde5871935281007be003fc796d7714df
shirleywbi <swwbi@msn.com>
2020-09-01 00:05:30 -0700

    add assets, old styling; fix project card image placement

---
+++ portfolio/src/components/primitives/ProjectCard.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ProjectModal from './ProjectModal';
+import { Card } from 'react-bootstrap';
+import '../../styles/ProjectCard.css';
+
+const ProjectCard = ({ project }) => {
+    const [modalShow, setModalShow] = useState(false);
+    const [showTitle, setShowTitle] = useState(false);
+    const { title, image } = project;
+
+    return <>
+        <ProjectModal
+            project={project}
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+        />
+        <div className={"project-card-container"}>
+            <Card
+                className={"project-card"}
+                onMouseEnter={() => setShowTitle(true)}
+                onMouseLeave={() => setShowTitle(false)}
+                onClick={() => setModalShow(true)}
+            >
+                {
+                    showTitle ?
+                    <Card.ImgOverlay className={"project-card-overlay"}>
+                        <h4>{title}</h4>
+                    </Card.ImgOverlay> : null
+                }
+                <Card.Img 
+                    className={"image-project-card"} 
+                    src={image} 
+                    alt=""
+                    variant="top"
+                />
+            </Card>
+        </div>
+    </>
+}
+
+export default ProjectCard;
